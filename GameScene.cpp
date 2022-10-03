40c6129f6cb4d6676769e616fe1eace0207c247f
takei_yuushin0831 <k021g1262@g.neec.ac.jp>
2022-10-03 11:00:46 +0900

    プロジェクト ファイルを追加します。

---
+++ GameScene.cpp
@@ -0,0 +1,145 @@
+﻿#include "GameScene.h"
+#include <cassert>
+
+using namespace DirectX;
+
+GameScene::GameScene()
+{
+}
+
+GameScene::~GameScene()
+{
+	delete spriteBG;
+	delete object3d;
+
+	delete(sprite1);
+	delete(sprite2);
+}
+
+void GameScene::Initialize(DirectXCommon* dxCommon, Input* input)
+{
+	// nullptrチェック
+	assert(dxCommon);
+	assert(input);
+
+	this->dxCommon = dxCommon;
+	this->input = input;
+
+	// デバッグテキスト用テクスチャ読み込み
+	Sprite::LoadTexture(debugTextTexNumber, L"Resources/debugfont.png");
+	// デバッグテキスト初期化
+	debugText.Initialize(debugTextTexNumber);
+
+	// テクスチャ読み込み
+	Sprite::LoadTexture(1, L"Resources/background.png");
+	Sprite::LoadTexture(2, L"Resources/texture.png");
+
+	sprite1 = Sprite::Create(2, { 0,0 });
+
+	sprite2 = Sprite::Create(2, { 500,500 }, { 1,0,0,1 }, { 0,0 });
+
+	// 背景スプライト生成
+	spriteBG = Sprite::Create(1, { 0.0f,0.0f });
+	// 3Dオブジェクト生成
+	object3d = Object3d::Create();
+	object3d->Update();
+}
+
+void GameScene::Update()
+{
+	// オブジェクト移動
+	if (input->PushKey(DIK_UP) || input->PushKey(DIK_DOWN) || input->PushKey(DIK_RIGHT) || input->PushKey(DIK_LEFT))
+	{
+		// 現在の座標を取得
+		XMFLOAT3 position = object3d->GetPosition();
+
+		// 移動後の座標を計算
+		if (input->PushKey(DIK_UP)) { position.y += 1.0f; }
+		else if (input->PushKey(DIK_DOWN)) { position.y -= 1.0f; }
+		if (input->PushKey(DIK_RIGHT)) { position.x += 1.0f; }
+		else if (input->PushKey(DIK_LEFT)) { position.x -= 1.0f; }
+
+		// 座標の変更を反映
+		object3d->SetPosition(position);
+
+		if (input->PushKey(DIK_SPACE))
+		{
+			XMFLOAT2 position = sprite1->GetPosition();
+
+			position.x += 1.0f;
+
+			sprite1->SetPosition(position);
+		}
+	}
+
+	// カメラ移動
+	if (input->PushKey(DIK_W) || input->PushKey(DIK_S) || input->PushKey(DIK_D) || input->PushKey(DIK_A))
+	{
+		if (input->PushKey(DIK_W)) { Object3d::CameraMoveVector({ 0.0f,+1.0f,0.0f }); }
+		else if (input->PushKey(DIK_S)) { Object3d::CameraMoveVector({ 0.0f,-1.0f,0.0f }); }
+		if (input->PushKey(DIK_D)) { Object3d::CameraMoveVector({ +1.0f,0.0f,0.0f }); }
+		else if (input->PushKey(DIK_A)) { Object3d::CameraMoveVector({ -1.0f,0.0f,0.0f }); }
+	}
+
+	object3d->Update();
+}
+
+void GameScene::Draw()
+{
+	//描画
+	sprite1->Draw();
+	sprite2->Draw();
+
+
+
+
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* cmdList = dxCommon->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+	// 背景スプライト描画
+	spriteBG->Draw();
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Object3d::PreDraw(cmdList);
+
+	// 3Dオブクジェクトの描画
+	object3d->Draw();
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+	// 3Dオブジェクト描画後処理
+	Object3d::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(cmdList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText.DrawAll(cmdList);
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+#pragma endregion
+}
---
+++ Input.cpp
@@ -0,0 +1,65 @@
+﻿#include "Input.h"
+#include <cassert>
+
+#pragma comment(lib, "dinput8.lib")
+
+void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
+{
+	HRESULT result = S_FALSE;
+
+	// DirectInputオブジェクトの生成	
+	result = DirectInput8Create(hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&dinput, nullptr);
+	assert(SUCCEEDED(result));
+
+	// キーボードデバイスの生成	
+	result = dinput->CreateDevice(GUID_SysKeyboard, &devkeyboard, NULL);
+	assert(SUCCEEDED(result));
+
+	// 入力データ形式のセット
+	result = devkeyboard->SetDataFormat(&c_dfDIKeyboard); // 標準形式
+	assert(SUCCEEDED(result));
+
+	// 排他制御レベルのセット
+	result = devkeyboard->SetCooperativeLevel(hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
+	assert(SUCCEEDED(result));
+
+}
+
+void Input::Update()
+{
+	devkeyboard->Acquire();	// キーボード動作開始
+
+	// 前回のキー入力を保存
+	memcpy(keyPre, key, sizeof(key));
+
+	// キーの入力
+	devkeyboard->GetDeviceState(sizeof(key), key);
+}
+
+bool Input::PushKey(BYTE keyNumber)
+{
+	// 異常な引数を検出
+	assert(0 <= keyNumber && keyNumber <= 256);
+
+	// 0でなければ押している
+	if (key[keyNumber]) {
+		return true;
+	}
+
+	// 押していない
+	return false;
+}
+
+bool Input::TriggerKey(BYTE keyNumber)
+{
+	// 異常な引数を検出
+	assert(0 <= keyNumber && keyNumber <= 256);
+
+	// 前回が0で、今回が0でなければトリガー
+	if (!keyPre[keyNumber] && key[keyNumber]) {
+		return true;
+	}
+
+	// トリガーでない
+	return false;
+}
